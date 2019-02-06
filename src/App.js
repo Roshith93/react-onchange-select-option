a2a9341312c87b4df8205861c43585a5897a35c2
Nagabhushana Hiremath <nagabhushana@spotsoon.com>
2019-02-06 10:40:13 +0530

    Initial blood
    onchange select option from external api
    
    have to add more features on onchange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    results: [],
+    selectedItem: "",
+  };
+  componentDidMount() {
+    
+    Axios('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        this.setState({
+          results: res.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    
+  }
   render() {
+    console.log(this.state)
+    let userResults = this.state.results.map(result => <option key={result.id}>{result.name}</option>)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <h1>onchange</h1>
+         <select onChange={e => this.setState({selectedItem: e.target.value})}>
+         {userResults}
+        </select>
+        <p>selectedItem : {this.state.selectedItem}</p>
       </div>
     );
   }
